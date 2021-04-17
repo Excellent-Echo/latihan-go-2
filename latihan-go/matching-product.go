1e01e196f00058a0852af61d2575140671719aca
Angga Prasetya W <agpw.id@gmail.com>
2021-04-17 18:10:10 +0800

    add comment

--- latihan-go/matching-product.go
+++ latihan-go/matching-product.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	array := [...]int{10, 10, 20, 20, 10, 30, 50, 10, 20}
-	// array := [...]int{1, 3, 3, 4, 5, 6, 1}
-	// array := [...]int{67, 213, 1, 0, 98, 2}
+	array := [...]int{10, 10, 20, 20, 10, 30, 50, 10, 20} // 5 ??
+	// array := [...]int{1, 3, 3, 4, 5, 6, 1} // 2
+	// array := [...]int{67, 213, 1, 0, 98, 2} // 0
 	count := 0
 
 	for i := 0; i < len(array); i++ {
