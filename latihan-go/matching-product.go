ca0b152daa78c64dbe7ca1b727d58a00194fc977
Angga Prasetya W <agpw.id@gmail.com>
2021-04-17 18:07:38 +0800

    wip matching-product.go (there is still a bug if the same value is more than 2)

--- latihan-go/matching-product.go
+++ latihan-go/matching-product.go
@@ -4,30 +4,18 @@ import "fmt"
 
 func main() {
 	array := [...]int{10, 10, 20, 20, 10, 30, 50, 10, 20}
-	// slice := []
-	var tempArray []int
-	var slice []int
-	var slicex []int
-	// var finalArray []int
-	var otherArray []int
+	// array := [...]int{1, 3, 3, 4, 5, 6, 1}
+	// array := [...]int{67, 213, 1, 0, 98, 2}
+	count := 0
 
-	for index, value := range array {
-		if value == array[index+2] {
-			tempArray = append(tempArray, value)
-			slice = array[array[index]-len(array):]
+	for i := 0; i < len(array); i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] == array[j] {
+				count++
+				break
+			}
 		}
-
-		// else {
-		// 	otherArray = append(otherArray, value)
-		// }
-
-		if len(tempArray) > 2 {
-			slicex = tempArray[:2]
-		}
-
 	}
-	fmt.Println(slice)
-	fmt.Println(slicex)
-	fmt.Println(tempArray)
-	fmt.Println(otherArray)
+
+	fmt.Println("output adalah", count)
 }
