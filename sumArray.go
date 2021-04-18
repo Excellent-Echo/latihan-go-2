75a7edf89abf6741b83fb502b1ef0e8bfbb9b296
danangtomo <ajidanang9@gmail.com>
2021-04-18 23:28:09 +0700

    sum array

---
+++ sumArray.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	array := []int{3, 3, 8, 3, 8, 2}
+	hasilPenjumlahan := 0
+
+	for _, num := range array {
+		hasilPenjumlahan += num
+	}
+	fmt.Println(hasilPenjumlahan)
+}
