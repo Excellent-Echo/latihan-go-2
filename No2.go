ab2b486f8464f2f35e32a9aeff2aa490acc22eaf
Ah Rifqi Ubaidilah <42555735+rifqi23ahmad@users.noreply.github.com>
2021-04-18 21:19:21 +0700

    reverse finish

--- No2.go
+++ No2.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	str := "Ah \n Rifqi"
+	str := "Ah Rifqi"
 	slice := make([]string, 0)
 	for i := len(str) - 1; i >= 0; i-- {
 		if string(str[i]) == string(" ") {
