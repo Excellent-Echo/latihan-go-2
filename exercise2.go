df4ec355538abae940c1ed619dcb3d902af81893
efzedel16 <efzedel16@outlook.com>
2021-04-23 02:19:39 +0700

    Update exercise2.go

--- exercise2.go
+++ exercise2.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	str := "impact byte"
+	var str string
+
+	fmt.Printf("Masukan: ")
+	fmt.Scanf("%s", &str)
+
 	char := make([]string, 0)
 
 	for i := len(str) - 1; i >= 0; i-- {
 		if string(str[i]) == (" ") {
 			continue
 		} else {
-			char = append(char, string(str[i]))
+			char = append(char, string(str[i]), ",")
 		}
 	}
 	fmt.Println(char)
